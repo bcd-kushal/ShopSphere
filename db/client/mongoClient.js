0fe3720f17a4ce5d7676ced714bcceb130c1ea26
Kushal <dev@kushalkumarsaha.com>
2024-03-06 00:00:25 +0530

    Delete db directory

--- db/client/mongoClient.js
+++
@@ -1,2 +0,0 @@
-const ATALS_URI = process.env.ATALS_URI;
-export default ATALS_URI;
--- db/defaults/defaults.js
+++
@@ -1,7 +0,0 @@
-const DEFAULTS = {
-    "range": [0,15],
-    "sort": "ascending",
-    "limit": 15
-}
-
-export default DEFAULTS;
